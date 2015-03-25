9d7996b58476a8f8ba425ef4533c5d619211b1b2
alrikolson <alrik.olson@oregonstate.edu>
2015-03-24 20:08:34 -0700

    test
    test

--- ThreatMeter/ThreatMeter/Program.cs
+++ ThreatMeter/ThreatMeter/Program.cs
@@ -10,7 +10,7 @@ namespace ThreatMeter
     {
         static void Main(string[] args)
         {
-            new Tm();
+            ThreatMeter.Load();
         }
     }
 }
--- ThreatMeter/ThreatMeter/ThreatMeter.cs
+++ ThreatMeter/ThreatMeter/ThreatMeter.cs
@@ -1,22 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
 
 namespace ThreatMeter
 {
-    internal class Tm
+    internal static class ThreatMeter
     {
-        public static Obj_AI_Hero Player;
+        internal static Menu Config;
+        internal static Orbwalking.Orbwalker Orbwalker;
+        internal static Obj_AI_Hero Player;
 
-        public Tm()
+        internal static void Load()
         {
-            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
-        }
+            try
+            {
+                Player = ObjectManager.Player;
 
-        private static void Game_OnGameLoad(EventArgs args)
-        {
+                //Print the welcome message
+                Game.PrintChat("Threat Meter Loaded!");
 
+                //Load the menu.
+                Config = new Menu("ThreatMeter", "ThreatMeter", true);
+
+                //Add the target selector.
+                //TargetSelector.AddToMenu(Config.SubMenu("Selector"));
+
+                //Add the orbwalking.
+                //Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalker"));
+
+                //Load the crosshair
+                //Crosshair.Load();
+
+                //Check if the champion is supported
+                //try
+                //{
+                //    var type = Type.GetType("ProSeries.Champions." + Player.ChampionName);
+                //    if (type != null)
+                //        type.GetMethod("Load").Invoke(null, null);
+                //}
+                //catch (NullReferenceException)
+                //{
+                //    Game.PrintChat(Player.ChampionName + " is not supported yet! however the orbwalking will work");
+                //}
+
+                //Add ADC items usage.
+                //ItemManager.Load();
+
+                //Add the menu as main menu.
+                Config.AddToMainMenu();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
-
 }
