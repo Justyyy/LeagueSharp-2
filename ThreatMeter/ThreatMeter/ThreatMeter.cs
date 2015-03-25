1317f2340f618807fd4e07b9d3b7e084d2e4fd3b
alrikolson <alrik.olson@oregonstate.edu>
2015-03-24 19:55:06 -0700

    threatmeter

---
+++ ThreatMeter/ThreatMeter/ThreatMeter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace ThreatMeter
+{
+    internal class ThreatMeter
+    {
+        private static Obj_AI_Hero _player;
+        public ThreatMeter()
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;    
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            _player = ObjectManager.Player;
+            Game.PrintChat("ThreatMeter loaded.");
+        }
+
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (Game.Time % 2 == 0)
+            {
+                Game.PrintChat(_player.ChampionName);
+            }
+        }
+    }
+}
