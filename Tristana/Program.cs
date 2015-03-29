d52e2e115d7d781f3aec03c7ae3d75440352a8af
alrikolson <alrik.olson@oregonstate.edu>
2015-03-28 22:45:55 -0700

    trsdt

--- Tristana/Program.cs
+++ Tristana/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
@@ -51,13 +52,59 @@ namespace Tristana
             Config.AddSubMenu(new Menu("Misc", "Misc"));
             Config.SubMenu("Misc").AddItem(new MenuItem("UseAntiGapcloser", "R on Gapclose").SetValue(true));
 
-            AntiGapcloser.OnEnemyGapcloser += OnEnemyGapcloser;
+            var drawMenu = new Menu("Drawing", "Drawing");
+            {
+                drawMenu.AddItem(new MenuItem("Draw_Disabled", "Disable All").SetValue(false));
+                drawMenu.AddItem(new MenuItem("Draw_Q", "Draw Q").SetValue(true));
+                drawMenu.AddItem(new MenuItem("Draw_W", "Draw W").SetValue(true));
+                drawMenu.AddItem(new MenuItem("Draw_E", "Draw E").SetValue(true));
+                drawMenu.AddItem(new MenuItem("Draw_R", "Draw R").SetValue(true));
+                drawMenu.AddItem(new MenuItem("Draw_R_Killable", "Draw R Mark on Killable").SetValue(true));
 
+                var drawComboDamageMenu = new MenuItem("Draw_ComboDamage", "Draw Combo Damage").SetValue(true);
+                var drawFill =
+                    new MenuItem("Draw_Fill", "Draw Combo Damage Fill").SetValue(new Circle(true,
+                        Color.FromArgb(90, 255, 169, 4)));
+                drawMenu.AddItem(drawComboDamageMenu);
+                drawMenu.AddItem(drawFill);
+                DamageIndicator.DamageToUnit = GetComboDamage;
+                DamageIndicator.Enabled = drawComboDamageMenu.GetValue<bool>();
+                DamageIndicator.Fill = drawFill.GetValue<Circle>().Active;
+                DamageIndicator.FillColor = drawFill.GetValue<Circle>().Color;
+                drawComboDamageMenu.ValueChanged +=
+                    delegate(object sender, OnValueChangeEventArgs eventArgs)
+                    {
+                        DamageIndicator.Enabled = eventArgs.GetNewValue<bool>();
+                    };
+                drawFill.ValueChanged +=
+                    delegate(object sender, OnValueChangeEventArgs eventArgs)
+                    {
+                        DamageIndicator.Fill = eventArgs.GetNewValue<Circle>().Active;
+                        DamageIndicator.FillColor = eventArgs.GetNewValue<Circle>().Color;
+                    };
+
+                Config.AddSubMenu(drawMenu);
+            }
+
+            AntiGapcloser.OnEnemyGapcloser += OnEnemyGapcloser;
+            
             Game.OnUpdate += Game_OnGameUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
             Orbwalking.BeforeAttack += Orbwalking_BeforeAttack;
             Config.AddToMainMenu();
             Game.PrintChat("'Rocket Girl Tristana' Loaded!");
 
+        }
+
+        private static float GetComboDamage(Obj_AI_Hero hero)
+        {
+            float comboDamage = 0;
+            return comboDamage;
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            throw new NotImplementedException();
         }
 
         private static void OnEnemyGapcloser(ActiveGapcloser gapcloser)
@@ -74,6 +121,17 @@ namespace Tristana
 
         private static void Game_OnGameUpdate(EventArgs args)
         {
+            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(h => h.IsEnemy))
+            {
+                foreach (var buff in hero.Buffs)
+                {
+                    Game.PrintChat("------------------");
+                    Game.PrintChat("Source: {0}", buff.SourceName);
+                    Game.PrintChat("Display: {0}", buff.DisplayName);
+                    Game.PrintChat("Name: {0}", buff.Name);
+                    Game.PrintChat("------------------");
+                }
+            }
             Q.Range = 541 + 9 * (_player.Level - 1);
             E.Range = 541 + 9 * (_player.Level - 1);
             R.Range = 541 + 9 * (_player.Level - 1);
